4018d2f68ba3964e8bf1da75deeabd70735e2450
MuazzezA <ayd.muazzez@gmail.com>
2025-04-21 23:57:30 +0300

    feat: upload image to filter screen

--- VisionCoreAnalyzer/Views/FilterViewController.swift
+++ VisionCoreAnalyzer/Views/FilterViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-class FilterViewController: UIViewController{
+class FilterViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
     
     @IBOutlet weak var filterLabel: AppLabel!
+    @IBOutlet weak var uploadImageButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     var selectedCategory: FilterCategory?
     
     override func viewDidLoad(){
@@ -18,5 +21,27 @@ class FilterViewController: UIViewController{
         if let category = selectedCategory {
             filterLabel.text = category.title
         }
+        
+    }
+    
+    @IBAction func uploadImageButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true){
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            imageView.image = image
+            uploadImageButton.setTitle("Change Image", for: .normal)
+        } else{
+            print("Upload image error")
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
